04d2cb45f4fb9b5e1bfbc64a842ff01cc33a598b
rafapoerner <rafapoerner@yahoo.com.br>
2024-03-05 21:49:14 -0300

    Mais ajustes de package

--- dist/painelalento/server/main.server.mjs
+++ dist/painelalento/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-FTEMZLNS.mjs";import"./chunk-3QZT5FJR.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-BSBMBPZH.mjs";import"./chunk-JRFMKOIU.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
