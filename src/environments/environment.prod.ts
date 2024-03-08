b9b72949dbaa1ef06ab065b7b28f1188c2b09922
rafapoerner <rafapoerner@yahoo.com.br>
2024-03-08 19:45:59 -0300

    Trocado servidor

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: "https://painelalentoapi.up.railway.app/"
+  apiUrl: "https://painelalentoapi.azurewebsites.net/"
 };
 
-// apiUrl: "https://painelalentoapi.azurewebsites.net/"
+// apiUrl: "https://painelalentoapi.up.railway.app/"
