1ac91c2c4bd2b7360237f0a16b2a7cfa4bca7e9d
rafapoerner <rafapoerner@yahoo.com.br>
2024-04-29 21:30:02 -0300

    Mudanças de environments

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: "https://painelalentoapi.up.railway.app/" 
+  apiUrl: "https://localhost:44316/" 
 };
 
 // apiUrl: 'https://painelalentoapi.azurewebsites.net/'
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: "https://painelalentoapi.up.railway.app/"
+  apiUrl: "https://localhost:44316/"
 };
 
 // apiUrl: "https://painelalentoapi.azurewebsites.net/"
