39ceb0cf41dcb3a5c13255342b841708ed28aebe
rafapoerner <rafapoerner@yahoo.com.br>
2024-04-30 17:25:47 -0300

    Mudanças pra railway.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: "https://localhost:44316/" 
+  apiUrl: "https://painelalentoapi.up.railway.app/" 
 };
 
+// apiUrl: "https://localhost:44316/" 
 // apiUrl: 'https://painelalentoapi.azurewebsites.net/'
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: "https://localhost:44316/"
+  apiUrl: "https://painelalentoapi.up.railway.app/"
 };
 
+// apiUrl: "https://localhost:44316/"
 // apiUrl: "https://painelalentoapi.azurewebsites.net/"
