8cfa95494da7e9aed18ed5bb406a51d7996292ad
rafapoerner <rafapoerner@yahoo.com.br>
2024-04-30 22:41:25 -0300

    Mudanças para localhost 2

--- dist/painelalento/server/main.server.mjs
+++ dist/painelalento/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-WY6IX7CU.mjs";import"./chunk-HT3LIRGT.mjs";import"./chunk-6IJN6NCK.mjs";export{a as default};
+import{a}from"./chunk-CPQ7IHJF.mjs";import"./chunk-HT3LIRGT.mjs";import"./chunk-6IJN6NCK.mjs";export{a as default};
