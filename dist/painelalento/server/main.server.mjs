088b2219fa9de5756a567ac2dfa1872f98a1f4e5
rafapoerner <rafapoerner@yahoo.com.br>
2024-05-06 21:36:17 -0300

    Troca de links do BI

--- dist/painelalento/server/main.server.mjs
+++ dist/painelalento/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-ZU6BQSRN.mjs";import"./chunk-HT3LIRGT.mjs";import"./chunk-6IJN6NCK.mjs";export{a as default};
+import{a}from"./chunk-VH4I4M7X.mjs";import"./chunk-HT3LIRGT.mjs";import"./chunk-6IJN6NCK.mjs";export{a as default};
