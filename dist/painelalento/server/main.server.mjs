70bae3bb62f856c364c6f71980069cb7ace3cfc6
Rafael Poerner <rafapoerner@yahoo.com.br>
2024-11-25 13:06:49 -0300

    Troca de IFrame de KPI de atualização

--- dist/painelalento/server/main.server.mjs
+++ dist/painelalento/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-TGDCHTUS.mjs";import"./chunk-H65J6PTM.mjs";import"./chunk-6IJN6NCK.mjs";export{a as default};
+import{a}from"./chunk-2VKJ3CDN.mjs";import"./chunk-H65J6PTM.mjs";import"./chunk-6IJN6NCK.mjs";export{a as default};
